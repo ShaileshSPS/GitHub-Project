0c64f0e7804a9767422348a1145a572c14114e47
Shailesh <shailesh.shanbhags@gmail.com>
2021-12-06 23:15:31 +0530

    axios call added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import SearchFilter from './components/SearchAccounts';
 import './css/styles.css';
 
 function App() {
   return (
     <div className="App">
-      <SearchFilter/>
+      <SearchFilter />
     </div>
   );
 }
--- src/components/SearchAccounts.js
+++ src/components/SearchAccounts.js
@@ -1,87 +1,118 @@
-import React, { Component } from "react";
-import { MDBRow, MDBCol, MDBInput, MDBBtn } from "mdbreact";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import $ from "jquery";
+import "jquery-ui/ui/widgets/accordion";
+import "jquery-ui/themes/base/all.css";
+toast.configure();
 
-export default class SearchFilter extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            content: '',
-            data: {}
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmitForm = this.handleSubmitForm.bind(this);
-    }
-
-    handleChange = (e) => {
-        this.setState({
-            content: e.target.value
-        })
-    }
-
-    handleSubmitForm(event) {
-        event.preventDefault();
-        axios.get(`https://api.github.com/search/repositories?q=user:${this.state.content}+is:public`)
+const SearchFilter = () => {
+    const [user, setUser] = useState({
+        username: "",
+        data: {}
+    });
+    const onInputChange = e => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+    const onSubmit = async e => {
+        e.preventDefault();
+        await axios.get(`https://api.github.com/search/repositories?q=user:${user.username}+is:public`)
             .then(response => {
-                this.setState({
+                setUser({
                     data: response.data.items
                 })
             }).catch(err => {
                 console.log(err)
             })
     }
+    const { username } = user;
+    const ResultHtml = () => {
+        var flag = true;
+        if (user.username === "") {
+            flag = false;
+            $(".result-row").hide();
+            return (
+                <div></div>
+            )
+        }
+        else {
+            $(".result-row").show();
+        }
+        if (flag) {
+            var result = user.data;
+            const postList = result.length ? (
+                result.map(post => {
+                    const date = new Date(post.updated_at);
 
-    render() {
-        let result = this.state.data;
-        const postList = result.length ? (
-            result.map(post => {
-                const  date = new Date(post.updated_at);
+                    var hours = date.getHours();
+                    var minutes = date.getMinutes();
+                    var ampm = hours >= 12 ? 'PM' : 'AM';
+                    hours = hours % 12;
+                    hours = hours ? hours : 12;
+                    minutes = minutes < 10 ? '0' + minutes : minutes;
+                    var strTime = hours + ':' + minutes + '' + ampm;
 
-                var hours = date.getHours();
-                var minutes = date.getMinutes();
-                var ampm = hours >= 12 ? 'PM' : 'AM';
-                hours = hours % 12;
-                hours = hours ? hours : 12;
-                minutes = minutes < 10 ? '0'+minutes : minutes;
-                var strTime = hours + ':' + minutes + '' + ampm;
-               
-                const res = date.getDate() +'/' + (date.getMonth()+1) + '/'+ date.getFullYear() + ', '+ strTime;
-                return (
-                    <div className="col-md-2 card-container" key={post.id}>
-                        <div className="card">
-                            <div className="card-content">
-                                <span className="card-title">{post.name}</span>
-                                <hr />
-                                <p className="card-description">{post.description}</p>
-                                <p className="card-date">{res
-                                }</p>
+                    const res = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ', ' + strTime;
+
+                    return (
+                        <div className="text-center">
+                            <div className="col-md-2 card-container" key={post.id}>
+                                <div className="card">
+                                    <div className="card-content">
+                                        <span className="card-title">{post.name}</span>
+                                        <hr />
+                                        <p className="card-description">{post.description}</p>
+                                        <p className="card-date">{res}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })
-        ) : (
-                <MDBCol className="error-msg"><h1>No Github repositories....</h1></MDBCol>
-            )
-        return (
-            <div>
-                <MDBRow>
-                    <MDBCol className="col-md-2">
-                        <h3>Github Demo</h3>
-                    </MDBCol>
-                    <MDBCol className="col-md-2 offset-md-7">
-                        <MDBInput label="Github user ID" outline onChange={this.handleChange} />
-                    </MDBCol>
-                    <MDBCol className="col-md-1">
-                        <MDBBtn className="btn btn-sm" type="submit" onClick={this.handleSubmitForm}>Search</MDBBtn>
-                    </MDBCol>
-                </MDBRow>
-                <hr />
-                <div className="row" style={{ marginTop: "1rem" }}>
-                    {postList}
+                    )
+                })
+            ) : (
+                <div className="text-center">
+                    <h1>No record found</h1>
                 </div>
-            </div>
-        );
+            )
+            return (
+                <div>
+                    <postList />
+                </div>
+            )
+        }
     }
+
+    return (
+        <div className="container">
+            <div className="w-75 mx-auto mt-5 shadow p-1">
+                <form >
+                    <h2 className="text-center">Search A User</h2>
+                    <div className="row">
+                        <div className="col-8">
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter"
+                                    name="username"
+                                    value={username}
+                                    onChange={e => onInputChange(e)}
+                                />
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <button className="btn btn-primary" onClick={e => onSubmit(e)}>Search</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div className="w-75 mx-auto mt-5 shadow p-1 result-row">
+                <ResultHtml />
+            </div>
+        </div>
+    );
 }
+
+export default SearchFilter;
